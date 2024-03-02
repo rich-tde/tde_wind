641c3720a2cd7ee6b5ed1b8213caaee46db6d3df
spaceturtle707 <p.martire98@gmail.com>
2024-03-02 14:54:32 +0100

    order

--- shock_zone.py
+++ shock_zone.py
@@ -8,17 +8,25 @@ import matplotlib.pyplot as plt
 import math
 from Utilities.operators import make_tree, calc_grad, calc_div
 
-gamma = 5/3
+##
+# PARAMETERS
+##
+
+gamma_chosen = 5/3
 mach_min = 1.3
 save = True
 
+##
+# FUNCTIONS
+##
+
 def temperature_bump(mach, gamma):
-    """ T_post/ T_pre shock """
+    """ T_post/ T_pre shock according to RH conditions. """
     Tbump =  (mach**2 * (gamma-1) + 2) * (2 * gamma * mach**2 - (gamma-1)) / (mach**2 * (gamma+1)**2)
     return Tbump
 
 def pressure_bump(mach, gamma):
-    """ P_post/ P_pre shock"""
+    """ P_post/ P_pre shock according to RH conditions. """
     Pbump = (2 * gamma * mach**2 - (gamma-1)) / (gamma+1)
     return Pbump
 
@@ -26,8 +34,8 @@ def shock_direction(sim_tree, X, Y, Z, Temp, point, delta):
     """ Find shock direction according eq.(5) by Schaal14 in the point of coordinates indices idx.
     Parameters
     -----------
-    sim_tree: nDarray.
-            Tree where to search the point.
+    sim_tree: tree.
+            Simulation points.
     X, Y, Z, Temp: arrays.
             Coordinates and temperature of the points of the tree.
     point: array.
@@ -42,7 +50,8 @@ def shock_direction(sim_tree, X, Y, Z, Temp, point, delta):
     grad = calc_grad(sim_tree, X, Y, Z, Temp, point, delta)
     magnitude = np.linalg.norm(grad)
     
-    if np.logical_and(np.abs(grad[0])<0.1, np.logical_and(np.abs(grad[1])<0.1, np.abs(grad[2])<0.1)):
+    # filter little numerical fluctuations
+    if np.logical_and(np.abs(grad[0])<1, np.logical_and(np.abs(grad[1])<1, np.abs(grad[2])<1)):
         ds = [0, 0, 0]
     else:
         ds = - np.divide(grad,magnitude)
@@ -52,10 +61,10 @@ def condition3(sim_tree, Press, Temp, point, ds, mach_min, gamma, delta):
     """ Last condition fot shock zone by Schaal14 .
     Parameters
     -----------
-    sim_tree: nDarray.
-            Tree where to search the point.
-    X, Y, Z, Press, Temp: arrays.
-            Coordinates, pressure and temperature of the points of the tree.
+    ssim_tree: tree.
+            Simulation points.
+    Press, Temp: arrays.
+            Pressure and temperature of the points of the tree.
     point: array.
             Starting point.
     ds: array (1x3)
@@ -117,3 +126,46 @@ def shock_zone(sim_tree, X, Y, Z, Den, Temp, Vx, Vy, Vz, point, delta, cond3, ch
         return cond1
     else:
         return False
+
+if __name__ == '__main__':
+    sim_tree, X, Y, Z, Vol, VX, VY, VZ, Den, P, Temp = make_tree('data_sim')
+
+    # Step for the gradient in the shock direction
+    dim_cell = (3*Vol/(4*np.pi))**(1/3)
+    step_dir = 2 * np.max(dim_cell)
+
+    # Step for the shock zone/surface
+    step_zone = 2 * np.min(dim_cell)
+
+    # Find cells in the shock zone. Store the data 
+    are_u_shock = np.zeros(len(X), dtype = bool)
+    shock_dirx = []
+    shock_diry = []
+    shock_dirz = []
+    X_shock = []
+    Y_shock = []
+    Z_shock = []
+    div_shock = []
+    T_shock = []
+
+    for i in range(len(X)):
+        point = [X[i],Y[i],Z[i]]
+        ds = shock_direction(sim_tree, X, Y, Z, Temp, point, step_dir)
+        if math.isnan(np.linalg.norm(ds)):
+            are_u_shock[i] = False
+            continue
+        cond3 = condition3(sim_tree, P, Temp, point, ds, mach_min, gamma_chosen, step)
+        shock = shock_zone(sim_tree, X, Y, Z, Den, Temp, VX, VY, VZ, point, step, cond3)
+        
+        if shock != False:
+            X_shock.append(X[i])
+            Y_shock.append(Y[i])
+            Z_shock.append(Z[i])
+            shock_dirx.append(ds[0])
+            shock_diry.append(ds[1])
+            shock_dirz.append(ds[2])
+            div_shock.append(shock)
+            T_shock.append(Temp[i])
+            are_u_shock[i] = True
+        else:
+            are_u_shock[i] = shock
