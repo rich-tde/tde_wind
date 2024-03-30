ebd4064509a1d210a2e78d7c113ad8532399db66
spaceturtle707 <p.martire98@gmail.com>
2024-03-30 19:40:30 +0100

    from RICH to AREPO

--- Utilities/xyz_extractor.py
+++ Utilities/xyz_extractor.py
@@ -1,7 +1,7 @@
 """
 Created on Fri Feb 24 17:06:56 2023
 
-@author: konstantinos
+@author: konstantinos, paola 
 """
 import sys
 sys.path.append('/Users/paolamartire/shocks')
@@ -11,7 +11,6 @@ import h5py
 from datetime import datetime
 import os
 
-#%% Get Densities
 
 ## File structure is
 # box, cycle, time, mpi, rank0 ... rank99.
@@ -41,7 +40,6 @@ def extractor(filename):
             # Store the length of the dataset
             lengths.append(len(f[key]['X']))
     
-    # Use lists for clarity
     X = []
     Y = []
     Z = []
@@ -67,7 +65,7 @@ def extractor(filename):
             # Timing
             end_time = datetime.now()
             print('Duration: {}'.format(end_time - start_time))
-            # For some reason, having the collumns into variables is way faster.
+
             x_data = f[key]['CMx']
             y_data = f[key]['CMy']
             z_data = f[key]['CMz']
@@ -83,7 +81,7 @@ def extractor(filename):
             T_data = f[key]['Temperature']
             P_data = f[key]['Pressure']
             # cs_data = f[key]['SoundSpeed']
-            star_data = f[key]['tracers']['Star']
+            # star_data = f[key]['tracers']['Star']
 
             for i in range(len(x_data)):
                 X.append(x_data[i])
@@ -99,31 +97,31 @@ def extractor(filename):
                 T.append(T_data[i])
                 P.append(P_data[i])
                 # cs.append(cs_data[i])
-                Star.append(star_data[i])
+                # Star.append(star_data[i]) #mass of the disrupted star for TDE
 
 
     # Close the file
     f.close()
-    return X, Y, Z, Den, Vx, Vy, Vz, Vol, Mass, IE, T, P, Star
+    return X, Y, Z, Den, Vx, Vy, Vz, Vol, Mass, IE, T, P
 
 if __name__ == '__main__':
     name = '196'
     path = f'TDE/{name}/'
-    X, Y, Z, Den, Vx, Vy, Vz, Vol, Mass, IE, T, P, Star = extractor(f'{path}snap_{name}.h5')
+    X, Y, Z, Den, Vx, Vy, Vz, Vol, Mass, IE, T, P = extractor(f'{path}snap_{name}.h5')
 
     # Save to another file.
-    np.save(f'{path}CMx_{name}', X)   
-    np.save(f'{path}CMy_{name}', Y) 
-    np.save(f'{path}CMz_{name}', Z) 
-    np.save(f'{path}Den_{name}', Den)
-    np.save(f'{path}Vx_{name}', Vx)   
-    np.save(f'{path}Vy_{name}', Vy) 
-    np.save(f'{path}Vz_{name}', Vz)
-    np.save(f'{path}Vol_{name}', Vol)
-    np.save(f'{path}Mass_{name}', Mass)   
-    np.save(f'{path}IE_{name}', IE) 
-    np.save(f'{path}T_{name}', T)
-    np.save(f'{path}P_{name}', P) 
-    #np.save(f'{path}cs_{name}', cs) 
-    np.save(f'{path}Star_{name}', Star) 
+    # np.save(f'{path}CMx_{name}', X)   
+    # np.save(f'{path}CMy_{name}', Y) 
+    # np.save(f'{path}CMz_{name}', Z) 
+    # np.save(f'{path}Den_{name}', Den)
+    # np.save(f'{path}Vx_{name}', Vx)   
+    # np.save(f'{path}Vy_{name}', Vy) 
+    # np.save(f'{path}Vz_{name}', Vz)
+    # np.save(f'{path}Vol_{name}', Vol)
+    # np.save(f'{path}Mass_{name}', Mass)   
+    # np.save(f'{path}IE_{name}', IE) 
+    # np.save(f'{path}T_{name}', T)
+    # np.save(f'{path}P_{name}', P) 
+    # np.save(f'{path}cs_{name}', cs) 
+    # np.save(f'{path}Star_{name}', Star) 
             
