713c3eb1a03a83154a92705d6e026ef153d80b30
spaceturtle707 <p.martire98@gmail.com>
2024-07-10 14:27:53 +0200

    move TZ section in plot.section

--- src/orbits.py
+++ src/orbits.py
@@ -216,9 +216,7 @@ def find_single_boundaries(x_data, y_data, z_data, dim_data, mass_data, stream, 
     w_params = np.array([width, ncells_w])
     h_params = np.array([height, ncells_h])
 
-    thresh_nocoeff = thresh/8
-
-    return indeces_boundary, x_T_width, w_params, h_params, thresh_nocoeff
+    return indeces_boundary, x_T_width, w_params, h_params, thresh
 
 def follow_the_stream(x_data, y_data, z_data, dim_data, mass_data, path, params):
     """ Find width and height all along the stream """
@@ -357,7 +355,6 @@ if __name__ == '__main__':
         plt.show()
 
     if make_stream:
-
         x_stream, y_stream, z_stream = find_transverse_com(data.X, data.Y, data.Z, dim_cell, data.Den, data.Mass, theta_arr, params)
         np.save(f'/Users/paolamartire/shocks/data/{folder}/stream_{check}{snap}.npy', [theta_arr, x_stream, y_stream, z_stream])
         # streamold = np.load(f'/Users/paolamartire/shocks/data/{folder}/DIMstream_{check}{snap}.npy' )
@@ -377,7 +374,7 @@ if __name__ == '__main__':
         X_midplane, Y_midplane, Den_midplane, Mass_midplane = make_slices([data.X, data.Y, data.Den, data.Mass], midplane)
         file = f'/Users/paolamartire/shocks/data/{folder}/stream_{check}{snap}.npy' 
         stream, indeces_boundary, x_T_width, w_params, h_params, theta_arr  = follow_the_stream(data.X, data.Y, data.Z, dim_cell, data.Mass, path = file, params = params)
-        cm_x, cm_y, cm_z = stream[0], stream[1], stream[2]
+        cm_x, cm_y, cm_z = stream[1], stream[2], stream[3]
         low_x, low_y = data.X[indeces_boundary[:,0]] , data.Y[indeces_boundary[:,0]]
         up_x, up_y = data.X[indeces_boundary[:,1]] , data.Y[indeces_boundary[:,1]]
 
@@ -395,149 +392,3 @@ if __name__ == '__main__':
         plt.ylim(-40,60)
         plt.savefig(f'/Users/paolamartire/shocks/Figs/{folder}/{check}/stream{snap}.png', dpi=300)
         plt.show()
-#%%
-    if TZslice:
-        indeces_min = np.argmin(np.abs(theta_arr+2.5))
-        indeces_max = np.argmin(np.abs(theta_arr+1))
-        indeces = [30, 80, 90, 100, 110, 120, 130, 140, 160, 200] #np.arange(indeces_min, indeces_max) #
-        stream = np.load(f'/Users/paolamartire/shocks/data/{folder}/stream_{check}{snap}.npy')
-        x_orbit, y_orbit, z_orbit = stream[1], stream[2], stream[3]
-        
-        plt.plot(x_orbit, y_orbit, c = 'k')
-        plt.scatter([x_orbit[i] for i in indeces], [y_orbit[i] for i in indeces], c = 'r')
-        plt.xlim(-300,30)
-        plt.ylim(-80,80)
-        plt.grid()
-        plt.savefig(f'/Users/paolamartire/shocks/Figs/{folder}/{check}/-2.4.png')
-        plt.show()  
-
-        for idx in indeces:
-            condition_tra, x_onplane, x0 = transverse_plane(data.X, data.Y, data.Z, dim_cell, x_orbit, y_orbit, z_orbit, idx, coord= True)
-            X_tra, Y_tra, Z_tra, Den_tra, Mass_tra = \
-                make_slices([data.X, data.Y, data.Z, data.Den, data.Mass], condition_tra)
-            indeces_boundary, x_T_width, w_params, h_params, thresh = find_single_boundaries(data.X, data.Y, data.Z, dim_cell, data.Mass, indeces_orbit, idx, params)
-
-            img1 = plt.scatter(x_onplane, Z_tra, c = Mass_tra,  cmap = 'viridis', s = 27)
-            cbar1 = plt.colorbar(img1)
-            cbar1.set_label(r'Mass', fontsize = 16)
-            plt.scatter(0, z_orbit[idx], marker = 'x', s = 37, c = 'k', alpha = 1)
-
-            plt.axvline(x_T_width[0], c = 'r')
-            plt.axvline(x_T_width[1], c = 'r') # T coordinates for width
-            plt.axvline(-thresh, linestyle = 'dashed', c = 'k', alpha = 0.8)
-            plt.axvline(thresh, linestyle = 'dashed', c = 'k', alpha = 0.8)
-            plt.text(0.9*thresh, -11*thresh, r'$(R/R_p)^{1/2} := K$',fontsize = 11, rotation = 90,  bbox=dict(facecolor='white', alpha=0.8))
-            plt.axvline(8*thresh, c = 'b', alpha = 0.8)
-            plt.axvline(-8*thresh, c = 'b', alpha = 0.8)
-            # text color blue
-            plt.text(8*thresh, -11*thresh, r'my boundary: 8K', fontsize = 12, rotation = 90, bbox=dict(facecolor='white', alpha=0.8), color = 'b')
-            plt.xlim(-14*thresh, 14*thresh)
-
-            plt.axhline(data.Z[indeces_boundary[2]], c = 'r')
-            plt.axhline(data.Z[indeces_boundary[3]], c = 'r')
-            plt.axhline(-thresh, linestyle = 'dashed', c = 'k', alpha = 0.8)
-            plt.axhline(thresh, linestyle = 'dashed', c = 'k', alpha = 0.8)
-            plt.axhline(-8*thresh, c = 'b', alpha = 0.8)
-            plt.axhline(8*thresh, c = 'b', alpha = 0.8)
-            plt.ylim(-14*thresh, 14*thresh)
-
-            plt.ylabel(r'Z [$R_\odot$]', fontsize = 18) 
-            plt.suptitle(f'{check}, cells W: {int(w_params[1])}, cells H: {int(h_params[1])}, theta: {np.round(theta_arr[idx], 2)}', fontsize = 16)
-            plt.tight_layout()
-            plt.savefig(f'/Users/paolamartire/shocks/Figs/{folder}/{check}/TZMass_slice{snap}_{idx}.png', dpi = 300)
-            plt.show()
-
-    if compare:
-        from Utilities.time_extractor import days_since_distruption
-        file = f'data/{folder}/stream_{check}{snap}_{step}.npy' 
-        indeces_stream, indeces_boundary, x_T_width, w_params, h_params, theta_arr  = follow_the_stream(data.X, data.Y, data.Z, dim_cell, data.Mass, path = file, params = params)
-        cm_x, cm_y = data.X[indeces_stream], data.Y[indeces_stream]
-        low_x, low_y = data.X[indeces_boundary[:,0]] , data.Y[indeces_boundary[:,0]]
-        up_x, up_y = data.X[indeces_boundary[:,1]] , data.Y[indeces_boundary[:,1]]
-        print('Low done')
-
-        data1 = make_tree(f'TDE/{folder}{check1}/{snap}', snap, is_tde = True, energy = False)
-        dim_cell1 = data1.Vol**(1/3)
-        file1 = f'data/{folder}/stream_{check1}{snap}_{step}.npy'
-        indeces_stream1, indeces_boundary1, x_T_width1, w_params1, h_params1, theta_arr1  = follow_the_stream(data1.X, data1.Y, data1.Z, dim_cell1, data1.Mass, path = file1, params = params)
-        cm_x1, cm_y1 = data1.X[indeces_stream1], data1.Y[indeces_stream1]
-        low_x1, low_y1 = data1.X[indeces_boundary1[:,0]] , data1.Y[indeces_boundary1[:,0]]
-        up_x1, up_y1 = data1.X[indeces_boundary1[:,1]] , data1.Y[indeces_boundary1[:,1]]
-        print('HiRes done')
-
-        # check2 = 'Res20'
-        # snap2 = '169'
-        # data2 = make_tree(f'TDE/{folder}{check2}/{snap2}', snap2, is_tde = True, energy = False)
-        # dim_cell2 = data2.Vol**(1/3)
-        # file2 = f'data/{folder}/stream_{check2}{snap2}_{step}.npy'
-        # indeces_stream2, indeces_boundary2, x_T_width2, w_params2, h_params2, theta_arr2  = follow_the_stream(data2.X, data2.Y, data2.Z, dim_cell2, data2.Mass, path = file2, params = params)
-        # print('Res20 done')
-
-        tfb = days_since_distruption(f'{path}/snap_{snap}.h5', m, mstar, Rstar, choose = 'tfb')
-        with open(f'data/{folder}/width_time{np.round(tfb,1)}.txt','a') as file:
-                # if file exist
-                file.write(f'# theta \n')
-                file.write((' '.join(map(str, theta_arr)) + '\n'))
-                file.write(f'# {check}, snap {snap} width \n')
-                file.write((' '.join(map(str, w_params[0])) + '\n'))
-                file.write(f'# {check}, snap {snap} Ncells \n')
-                file.write((' '.join(map(str, w_params[1])) + '\n'))
-                file.write(f'################################ \n')
-                file.write(f'# {check1}, snap {snap} width \n')
-                file.write((' '.join(map(str, w_params1[0])) + '\n'))
-                file.write(f'# {check1}, snap {snap} Ncells \n')
-                file.write((' '.join(map(str, w_params1[1])) + '\n'))
-                file.write(f'################################ \n')
-                # file.write(f'# {check2}, snap {snap2} width \n')
-                # file.write((' '.join(map(str, w_params2[0])) + '\n'))
-                # file.write(f'# {check2}, snap {snap2} Ncells \n')
-                # file.write((' '.join(map(str, w_params2[1])) + '\n'))
-                # file.write(f'################################ \n')
-                file.close()
-        
-        with open(f'data/{folder}/height_time{np.round(tfb,1)}.txt','a') as file:
-                # if file exist
-                file.write(f'# theta \n')
-                file.write((' '.join(map(str, theta_arr)) + '\n'))
-                file.write(f'# {check}, snap {snap} height \n')
-                file.write((' '.join(map(str, h_params[0])) + '\n'))
-                file.write(f'# {check}, snap {snap} Ncells \n')
-                file.write((' '.join(map(str, h_params[1])) + '\n'))
-                file.write(f'################################ \n')
-                file.write(f'# {check1}, snap {snap} height \n')
-                file.write((' '.join(map(str, h_params1[0])) + '\n'))
-                file.write(f'# {check1}, snap {snap} Ncells \n')
-                file.write((' '.join(map(str, h_params1[1])) + '\n'))
-                file.write(f'################################ \n')
-                # file.write(f'# {check2}, snap {snap2} height \n')
-                # file.write((' '.join(map(str, h_params2[0])) + '\n'))
-                # file.write(f'# {check2}, snap {snap2} Ncells \n')
-                # file.write((' '.join(map(str, h_params2[1])) + '\n'))
-                # file.write(f'################################ \n')
-                file.close()
-
-        #%%
-        plt.plot(cm_x, cm_y,  c = 'k', label = 'Orbit')
-        plt.plot(low_x, low_y, '--', c = 'k',label = 'Lower tube')
-        plt.plot(up_x, up_y, '-.', c = 'k', label = 'Upper tube')
-        plt.plot(cm_x1, cm_y1,  c = 'r', label = 'Orbit1')
-        plt.plot(low_x1, low_y1, '--', c = 'r',label = 'Lower tube1')
-        plt.plot(up_x1, up_y1, '-.', c = 'r', label = 'Upper tube1')
-        plt.xlabel(r'X [$R_\odot$]', fontsize = 18)
-        plt.ylabel(r'Y [$R_\odot$]', fontsize = 18)
-        plt.xlim(-100,30)
-        plt.ylim(-100,100)
-        plt.legend()
-        plt.show()
-
-        plt.plot(theta_arr, w_params[0], c= 'k', label = 'Low')
-        plt.plot(theta_arr1, w_params1[0], c='r', label = 'Middle')
-        # plt.plot(theta_arr2, w_params2[0], c='b', label = 'High')
-        plt.ylabel(r'Width [$R_\odot$]', fontsize = 18)
-        plt.xlabel(r'$\theta$', fontsize = 18)
-        plt.legend()
-        plt.ylim(0,10)
-        plt.grid()
-        plt.show()
-
-        
