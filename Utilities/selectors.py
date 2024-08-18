6660e7880c2efe43c5fd6445404d599bdc83be1d
spaceturtle707 <p.martire98@gmail.com>
2024-08-18 07:11:44 +0200

    alice selectors

--- Utilities/selectors.py
+++ Utilities/selectors.py
@@ -9,12 +9,12 @@ import os
 from Utilities.time_extractor import days_since_distruption
 
 def select_prefix(m, check, mstar, rstar, beta, n, compton = 'Compton'):
+    Mbh = 10**m
     if alice:
-        prepath = f'/home/martirep/data_pi-rossiem/TDE_data/'
-        Mbh = 10**m
         folder = f'R{rstar}M{mstar}BH{Mbh}beta{beta}S60n{n}{compton}'
+        prepath = f'/home/martirep/data_pi-rossiem/TDE_data/{folder}'
+        print(prepath)
     else: 
-        Mbh = 10**m
         folder = f'R{rstar}M{mstar}BH{Mbh}beta{beta}S60n{n}{check}/'
         prepath = f'/Users/paolamartire/shocks/TDE/{folder}'
     return prepath
