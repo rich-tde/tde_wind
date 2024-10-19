8faa091b5a915df4fabed0330fb52c85822e2bc8
spaceturtle707 <p.martire98@gmail.com>
2024-10-19 11:09:56 +0200

    fld curve

--- src/fld_curve.py
+++ src/fld_curve.py
@@ -15,7 +15,7 @@ warnings.filterwarnings('ignore')
 import csv
 
 import numpy as np
-import h5py
+# import h5py
 import healpy as hp
 import scipy.integrate as sci
 from scipy.interpolate import griddata
@@ -23,7 +23,7 @@ import matlab.engine
 from sklearn.neighbors import KDTree
 
 
-import Utilities.prelude as c
+import Utilities.prelude as prel
 from scipy.ndimage import uniform_filter1d # does moving mean without fucking the shape up
 # from Utilities.parser import parse
 from Utilities.selectors_for_snap import select_snap, select_prefix
@@ -50,8 +50,8 @@ Lphoto_all = np.zeros(len(snaps))
 
 #%% Opacities -----------------------------------------------------------------
 # Freq range
-f_min = c.Kb * 1e3 / c.h
-f_max = c.Kb * 3e13 / c.h
+f_min = prel.Kb * 1e3 / prel.h
+f_max = prel.Kb * 3e13 / prel.h
 f_num = 1_000
 frequencies = np.logspace(np.log10(f_min), np.log10(f_max), f_num)
 
@@ -95,9 +95,6 @@ _, _, plank2 = pad_interp(T_cool, Rho_cool, plank.T)
 # MATLAB GOES WHRRRR, thanks Cindy.
 eng = matlab.engine.start_matlab()
 
-if alice:
-    if check == 'Low':
-        check = ''
 pre = select_prefix(m, check, mstar, Rstar, beta, n, compton, step)
 print('we are in: ', pre)
 for idx_s, snap in enumerate(snaps):
@@ -112,10 +109,11 @@ for idx_s, snap in enumerate(snaps):
         Den = np.load(f'{pre}/snap_{snap}/Den_{snap}.npy')
         Rad = np.load(f'{pre}/snap_{snap}/Rad_{snap}.npy')
         Vol = np.load(f'{pre}/snap_{snap}/Vol_{snap}.npy')
-        with h5py.File(f'{pre}/snap_{snap}/snap_{snap}.h5', 'r') as fileh:
-            for i in range(len(box)):
-                box[i] = fileh['Box'][i]
-            fileh.close()
+        box = np.load(f'{pre}/snap_{snap}/box_{snap}.npy')
+        # with h5py.File(f'{pre}/snap_{snap}/snap_{snap}.h5', 'r') as fileh:
+        #     for i in range(len(box)):
+        #         box[i] = fileh['Box'][i]
+            # fileh.close()
         # day = np.loadtxt(f'{pre}/snap_{snap}/tbytfb_{snap}.txt')
     else:
         X = np.load(f'{pre}/{snap}/CMx_{snap}.npy')
@@ -125,37 +123,40 @@ for idx_s, snap in enumerate(snaps):
         Den = np.load(f'{pre}/{snap}/Den_{snap}.npy')
         Rad = np.load(f'{pre}/{snap}/Rad_{snap}.npy')
         Vol = np.load(f'{pre}/{snap}/Vol_{snap}.npy')
-        with h5py.File(f'{pre}/{snap}/snap_{snap}.h5', 'r') as fileh:
-            for i in range(len(box)):
-                box[i] = fileh['Box'][i]
-            fileh.close()
+        box = np.load(f'{pre}/{snap}/box_{snap}.npy')
     denmask = Den > 1e-19
     X, Y, Z, T, Den, Rad, Vol = make_slices([X, Y, Z, T, Den, Rad, Vol], denmask)
     Rad_den = np.multiply(Rad,Den) # now you have enrgy density
     del Rad   
-
     R = np.sqrt(X**2 + Y**2 + Z**2)
     #%% Cross dot -----------------------------------------------------------------
-    observers_xyz = hp.pix2vec(c.NSIDE, range(192))
+    observers_xyz = hp.pix2vec(prel.NSIDE, range(prel.NPIX))
     # Line 17, * is matrix multiplication, ' is .T
     observers_xyz = np.array(observers_xyz).T
     cross_dot = np.matmul(observers_xyz,  observers_xyz.T)
     cross_dot[cross_dot<0] = 0
-    cross_dot *= 4/192
+    cross_dot *= 4/prel.NPIX
 
     #%% Tree ----------------------------------------------------------------------
     #from scipy.spatial import KDTree
     xyz = np.array([X, Y, Z]).T
-    tree = eng.KDTreeSearcher(xyz)
     N_ray = 5_000
 
     # Flux?
-    F_photo = np.zeros((192, f_num))
-    F_photo_temp = np.zeros((192, f_num))
+    F_photo = np.zeros((prel.NPIX, f_num))
+    F_photo_temp = np.zeros((prel.NPIX, f_num))
 
     # Dynamic Box -----------------------------------------------------------------
-    reds = np.zeros(192)
-    for i in tqdm(range(192)):
+    reds = np.zeros(prel.NPIX)
+    time_start = 0
+    for i in range(prel.NPIX):
+        # Progress 
+        time_end = time.time()
+        print(f'Snap: {snap}, Obs: {i}', flush=False)
+        print(f'Time for prev. Obs: {(time_end - time_start)/60} min', flush = False)
+        time_start = time.time()
+        sys.stdout.flush()
+
         mu_x = observers_xyz[i][0]
         mu_y = observers_xyz[i][1]
         mu_z = observers_xyz[i][2]
@@ -189,12 +190,11 @@ for idx_s, snap in enumerate(snaps):
         y = r*mu_y
         z = r*mu_z
         xyz2 = np.array([x, y, z]).T
-        # del x, y, z
-        # tree = KDTree(xyz, leaf_size=50)
-        # _, idx = tree.query(xyz2, k=1)
-        idx = eng.knnsearch(tree, xyz2)
-        idx = [ int(idx[i][0] - 1) for i in range(len(idx))] # -1 because we start from 0
-        d = Den[idx] * c.den_converter
+        del x, y, z
+        tree = KDTree(xyz, leaf_size=50)
+        _, idx = tree.query(xyz2, k=1)
+        idx = [ int(idx[i][0]) for i in range(len(idx))] # no -1 because we start from 0
+        d = Den[idx] * prel.den_converter
         t = T[idx]
 
         # Interpolate ----------------------------------------------------------
@@ -205,24 +205,23 @@ for idx_s, snap in enumerate(snaps):
         sigma_plank = eng.interp2(T_cool2,Rho_cool2,plank2,np.log(t),np.log(d),'linear',0)
         sigma_plank = [sigma_plank[0][i] for i in range(N_ray)]
         sigma_plank_eval = np.exp(sigma_plank)
-        # del sigma_rossland, sigma_plank 
-        # gc.collect()
+        del sigma_rossland, sigma_plank 
+        gc.collect()
 
         # Optical Depth ---------------------------------------------------------------
         # Okay, line 232, this is the hard one.
         r_fuT = np.flipud(r.T)
         kappa_rossland = np.flipud(sigma_rossland_eval) 
-        los = - np.flipud(sci.cumulative_trapezoid(kappa_rossland, r_fuT, initial = 0)) * c.Rsol_cgs # dont know what it do but this is the conversion
+        los = - np.flipud(sci.cumulative_trapezoid(kappa_rossland, r_fuT, initial = 0)) * prel.Rsol_to_cm # dont know what it do but this is the conversion
         k_effective = np.sqrt(3 * np.flipud(sigma_plank_eval) * np.flipud(sigma_rossland_eval)) 
-        los_effective = - np.flipud(sci.cumulative_trapezoid(k_effective, r_fuT, initial = 0)) * c.Rsol_cgs
-        
+        los_effective = - np.flipud(sci.cumulative_trapezoid(k_effective, r_fuT, initial = 0)) * prel.Rsol_to_cm
+
         # Red -----------------------------------------------------------------------
         # Get 20 unique, nearest neighbors
         xyz3 = np.array([X[idx], Y[idx], Z[idx]]).T
-        # _, idxnew = tree.query(xyz3, k=20)
-        idxnew = eng.knnsearch(tree, xyz3, 'K', 20)
+        xyz3 = np.array([X[idx], Y[idx], Z[idx]]).T
+        _, idxnew = tree.query(xyz3, k=20)
         idxnew = np.unique(idxnew).T
-        idxnew = [ int(idxnew[i] -1) for i in range(len(idxnew))]
         dx = 0.5 * Vol[idx]**(1/3) # Cell radius
 
         # Get the Grads
@@ -236,6 +235,7 @@ for idx_s, snap in enumerate(snaps):
         gradx_m = griddata( f_inter_input, Rad_den[idxnew], method = 'linear',
                             xi = np.array([ X[idx]-dx, Y[idx], Z[idx]]).T )
         gradx = (gradx_p - gradx_m)/ (2*dx)
+        del gradx_p, gradx_m
 
         gradx = np.nan_to_num(gradx, nan =  0)
         grady_p = griddata( f_inter_input, Rad_den[idxnew], method = 'linear',
@@ -243,6 +243,7 @@ for idx_s, snap in enumerate(snaps):
         grady_m = griddata( f_inter_input, Rad_den[idxnew], method = 'linear',
                             xi = np.array([ X[idx], Y[idx]-dx, Z[idx]]).T )
         grady = (grady_p - grady_m)/ (2*dx)
+        del grady_p, grady_m
 
         grady = np.nan_to_num(grady, nan =  0)
 
@@ -253,41 +254,56 @@ for idx_s, snap in enumerate(snaps):
         # some nans here
         gradz_m = np.nan_to_num(gradz_m, nan =  0)
         gradz = (gradz_p - gradz_m)/ (2*dx)
+        del gradz_p, gradz_m
 
         grad = np.sqrt(gradx**2 + grady**2 + gradz**2)
         gradr = (mu_x * gradx) + (mu_y*grady) + (mu_z*gradz)
-        # del gradx, grady, gradz
-        # gc.collect()
-        
-        R_lamda = grad / ( c.Rsol_cgs * sigma_rossland_eval* Rad_den[idx])
+        del gradx, grady, gradz
+        gc.collect()
+
+        R_lamda = grad / ( prel.Rsol_to_cm * sigma_rossland_eval* Rad_den[idx])
         R_lamda[R_lamda < 1e-10] = 1e-10
         fld_factor = 3 * (1/np.tanh(R_lamda) - 1/R_lamda) / R_lamda 
-        smoothed_flux = -uniform_filter1d(r.T**2 * fld_factor * gradr / sigma_rossland_eval, 7) # i have removed the minus
-    
+        smoothed_flux = -uniform_filter1d(r.T**2 * fld_factor * gradr / sigma_rossland_eval, 7) # i have remov
+        
         try:
             b = np.where( ((smoothed_flux>0) & (los<2/3) ))[0][0] 
         except IndexError:
             b = 3117 # elad_b = 3117
-        Lphoto2 = 4*np.pi*c.c*smoothed_flux[b] * c.Msol_cgs / (c.tsol_cgs**2)
+        Lphoto2 = 4*np.pi*prel.c*smoothed_flux[b] * prel.Msol_to_g / (prel.t**2)
         EEr = Rad_den[idx]
         if Lphoto2 < 0:
-            Lphoto2 = 1e100 # it means that it will always pick max_length for the negatives, maybe this is what we are getting wrong
-        max_length = 4*np.pi*c.c*EEr[b]*r[b]**2 * c.Msol_cgs * c.Rsol_cgs / (c.tsol_cgs**2)
+            Lphoto2 = 1e100 # it means that it will always pick max_length for the negatives
+        max_length = 4*np.pi*prel.c*EEr[b]*r[b]**2 * prel.Msol_to_g * prel.Rsol_to_cm / (prel.t**2)
         Lphoto = np.min( [Lphoto2, max_length])
         reds[i] = Lphoto
-        # del smoothed_flux, R_lamda, fld_factor, EEr, los,
-        # gc.collect()
-    Lphoto_all[idx_s] = np.mean(reds) # save red
+        del smoothed_flux, R_lamda, fld_factor, EEr, los,
+        gc.collect()
+    Lphoto_snap = np.mean(reds)
+    Lphoto_all[idx_s] = Lphoto_snap # save red
+    # Lphoto_all[idx_s] = 4*np.pi*np.mean(reds) # save red
 
-# Lphoto_all[idx_s] = 4*np.pi*np.mean(reds) # save red
-    
-if save:
-    pre_saving = f'/data1/martirep/shocks/shock_capturing/data/{folder}/red'
-    if check == '':
-        check = 'Low'
-    with open(f'{pre_saving}/{check}_eladred.txt', 'a') as file:
-        file.write('# t/tfb \n' + ' '.join(map(str, tfb)) + '\n')
-        file.write('# FLD \n' + ' '.join(map(str, Lphoto_all)) + '\n')
+    # Save red of the single snap
+    if save:
+        pre_saving = f'/data1/martirep/shocks/shock_capturing/data/{folder}/red'
+        data = [snap, tfb[idx_s], Lphoto_snap]
+        if check == '':
+            print('change check')
+            check = 'Low'
+        with open(f'{pre_saving}/{check}_red.csv', 'a', newline='') as file:
+            writer = csv.writer(file)
+            writer.writerow(data)
         file.close()
-    print('saved dasy and red')
+
+# Save all days and red
+# if save:
+#     pre_saving = f'/data1/martirep/shocks/shock_capturing/data/{folder}/red'
+#     if check == '':
+#         print('change check')
+#         check = 'Low'
+#     with open(f'{pre_saving}/{check}_eladred.txt', 'a') as file:
+#         file.write('# t/tfb \n' + ' '.join(map(str, tfb)) + '\n')
+#         file.write('# FLD \n' + ' '.join(map(str, Lphoto_all)) + '\n')
+#         file.close()
+#     print('saved all days and red')
 eng.exit()
