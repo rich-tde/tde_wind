758d5121ff5682533d1b17360a5630d91f60843a
spaceturtle707 <p.martire98@gmail.com>
2024-11-19 09:44:11 +0100

    extract dissipation on alice

--- Utilities/simple_extractor.py
+++ Utilities/simple_extractor.py
@@ -31,6 +31,7 @@ def extractor(filename):
     # List with keys that don't hold relevant data
     not_ranks = ['Box', 'Cycle', 'Time', 'mpi']
     
+    box = np.zeros(6)
     X = []
     Y = []
     Z = []
@@ -46,8 +47,8 @@ def extractor(filename):
     P = []
     #cs = []
     Star = []
+    Diss = []
     Entropy = []
-    box = np.zeros(6)
     
     print('tot ranks: ', len(keys))
     # Iterate over ranks
@@ -61,46 +62,48 @@ def extractor(filename):
             else:
                 continue
         else:
-            print(key)
+            print(f[key].keys())
 
-            x_data = f[key]['CMx']
-            y_data = f[key]['CMy']
-            z_data = f[key]['CMz']
-            den_data = f[key]['Density']
+            # x_data = f[key]['CMx']
+            # y_data = f[key]['CMy']
+            # z_data = f[key]['CMz']
+            # den_data = f[key]['Density']
             
-            vx_data = f[key]['Vx']
-            vy_data = f[key]['Vy']
-            vz_data = f[key]['Vz']
-            vol_data = f[key]['Volume']
+            # vx_data = f[key]['Vx']
+            # vy_data = f[key]['Vy']
+            # vz_data = f[key]['Vz']
+            # vol_data = f[key]['Volume']
             
-            ie_data = f[key]['InternalEnergy']
-            rad_data = f[key]['Erad']
-            T_data = f[key]['Temperature']
-            P_data = f[key]['Pressure']
+            # ie_data = f[key]['InternalEnergy']
+            # rad_data = f[key]['Erad']
+            # T_data = f[key]['Temperature']
+            # P_data = f[key]['Pressure']
+            # star_data = f[key]['tracers']['Star']
+            Diss_data = f[key]['Dissipation']
+            # entropy_data = f[key]['tracers']['Entropy']
             #cs_data = f[key]['SoundSpeed']
-            star_data = f[key]['tracers']['Star']
-            entropy_data = f[key]['tracers']['Entropy']
             
-            for i in range(len(rad_data)):
-                X.append(x_data[i])
-                Y.append(y_data[i])
-                Z.append(z_data[i])
-                Den.append(den_data[i])
-                Vx.append(vx_data[i])
-                Vy.append(vy_data[i])
-                Vz.append(vz_data[i])
-                Vol.append(vol_data[i])
-                IE.append(ie_data[i])
-                Erad.append(rad_data[i])
-                Mass.append(vol_data[i] * den_data[i])
-                T.append(T_data[i])
-                P.append(P_data[i])
-                Star.append(star_data[i]) #mass of the disrupted star for TDE
-                Entropy.append(entropy_data[i])
+            for i in range(len(Diss_data)):
+                # X.append(x_data[i])
+                # Y.append(y_data[i])
+                # Z.append(z_data[i])
+                # Den.append(den_data[i])
+                # Vx.append(vx_data[i])
+                # Vy.append(vy_data[i])
+                # Vz.append(vz_data[i])
+                # Vol.append(vol_data[i])
+                # IE.append(ie_data[i])
+                # Erad.append(rad_data[i])
+                # Mass.append(vol_data[i] * den_data[i])
+                # T.append(T_data[i])
+                # P.append(P_data[i])
+                # Star.append(star_data[i]) #mass of the disrupted star for TDE
+                Diss.append(Diss_data[i])
+                # Entropy.append(entropy_data[i])
 
     # Close the file
     f.close()
-    return box, X, Y, Z, Den, Vx, Vy, Vz, Vol, Mass, IE, Erad, T, P, Star, Entropy
+    return Diss#box, X, Y, Z, Den, Vx, Vy, Vz, Vol, Mass, IE, Erad, T, P, Star, Diss, Entropy
 
 ##
 # MAIN
@@ -126,25 +129,27 @@ for snap in snaps:
         prepath = f'{prepath}/{snap}'
     file = f'{prepath}/snap_{snap}.h5'
     print(file)
-    box, X, Y, Z, Den, Vx, Vy, Vz, Vol, Mass, IE, Erad, T, P, Star, Entropy = extractor(file)
+    # box, X, Y, Z, Den, Vx, Vy, Vz, Vol, Mass, IE, Erad, T, P, Star, Diss, Entropy = extractor(file)
+    Diss = extractor(file)
 
     #%%
     # Save to another file.
-    np.save(f'{prepath}/box_{snap}', box) 
-    np.save(f'{prepath}/CMx_{snap}', X)   
-    np.save(f'{prepath}/CMy_{snap}', Y) 
-    np.save(f'{prepath}/CMz_{snap}', Z) 
-    np.save(f'{prepath}/Den_{snap}', Den)
-    np.save(f'{prepath}/Vx_{snap}', Vx)   
-    np.save(f'{prepath}/Vy_{snap}', Vy) 
-    np.save(f'{prepath}/Vz_{snap}', Vz)
-    np.save(f'{prepath}/Vol_{snap}', Vol)
-    np.save(f'{prepath}/Mass_{snap}', Mass)   
-    np.save(f'{prepath}/IE_{snap}', IE) 
-    np.save(f'{prepath}/T_{snap}', T)
-    np.save(f'{prepath}/P_{snap}', P) 
-    np.save(f'{prepath}/Star_{snap}', Star) 
-    np.save(f'{prepath}/Entropy_{snap}', Entropy) 
-    np.save(f'{prepath}/Rad_{snap}', Erad) 
+    # np.save(f'{prepath}/box_{snap}', box) 
+    # np.save(f'{prepath}/CMx_{snap}', X)   
+    # np.save(f'{prepath}/CMy_{snap}', Y) 
+    # np.save(f'{prepath}/CMz_{snap}', Z) 
+    # np.save(f'{prepath}/Den_{snap}', Den)
+    # np.save(f'{prepath}/Vx_{snap}', Vx)   
+    # np.save(f'{prepath}/Vy_{snap}', Vy) 
+    # np.save(f'{prepath}/Vz_{snap}', Vz)
+    # np.save(f'{prepath}/Vol_{snap}', Vol)
+    # np.save(f'{prepath}/Mass_{snap}', Mass)   
+    # np.save(f'{prepath}/IE_{snap}', IE) 
+    # np.save(f'{prepath}/Rad_{snap}', Erad) 
+    # np.save(f'{prepath}/T_{snap}', T)
+    # np.save(f'{prepath}/P_{snap}', P) 
+    # np.save(f'{prepath}/Star_{snap}', Star) 
+    np.save(f'{prepath}/Diss_{snap}', Diss)
+    # np.save(f'{prepath}/Entropy_{snap}', Entropy) 
     print('Done')
                 
