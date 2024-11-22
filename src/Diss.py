new correct apocentre calculation

src/Diss.py
@@ -27,12 +27,13 @@ mstar = .5
 Rstar = .47
 n = 1.5
 compton = 'Compton'
-check = 'LowRes'
+check = 'DoubleRad'
 Rt = Rstar * (Mbh/mstar)**(1/3)
 
 snaps, tfb = select_snap(m, check, mstar, Rstar, beta, n, compton, time = True) 
 
 Rdiss = np.zeros(len(snaps))
+Edisstot = np.zeros(len(snaps))
 for i,snap in enumerate(snaps):
     folder = f'R{Rstar}M{mstar}BH{Mbh}beta{beta}S60n{n}{compton}{check}'
     path = f'/home/martirep/data_pi-rossiem/TDE_data/{folder}/snap_{snap}'
@@ -41,6 +42,7 @@ for i,snap in enumerate(snaps):
     cut = data.Den > 1e-19
     Rsph, vol, Ediss_den = Rsph[cut], data.Vol[cut], data.Diss[cut]
     Ediss = Ediss_den * vol
+    Edisstot[i] = np.sum(Ediss)
 
     # Rdiss[i] = np.sum(Rsph * vol * Ediss) / np.sum(vol * Ediss)
 
@@ -49,7 +51,7 @@ for i,snap in enumerate(snaps):
 #     file.write(f'# Rdiss \n' + ' '.join(map(str, Rdiss)) + '\n')
 #     file.close()
 
-with open(f'{abspath}/data/{folder}/Rdiss_{check}.txt','a') as file:
+with open(f'{abspath}/data/{folder}/Ediss_{check}.txt','a') as file:
     file.write(f'# t/tfb \n' + ' '.join(map(str, tfb)) + '\n')
-    file.write(f'# Rdiss \n' + ' '.join(map(str, Ediss)) + '\n')
+    file.write(f'# Ediss \n' + ' '.join(map(str, Edisstot)) + '\n')
     file.close()

src/RShock.py
@@ -72,7 +72,10 @@ for j, check in enumerate(checks):
     dataLum = np.loadtxt(f'{abspath}/data/R{Rstar}M{mstar}BH{Mbh}beta{beta}S60n{n}{compton}{check}/{check}_red.csv', delimiter=',', dtype=float)
     snapsLum = dataLum[:, 0]
     tfbLum = dataLum[:, 1]   
-    Lum = dataLum[:, 2]   
+    Lum = dataLum[:, 2]  
+
+    # dataLum = np.loadtxt(f'{abspath}/data/R{Rstar}M{mstar}BH{Mbh}beta{beta}S60n{n}{compton}{check}/{check}_red.csv', delimiter=',', dtype=float)
+    # tfbLum, Lum = dataLum[0], dataLum[1] 
   
     mfall_yr = np.zeros(len(time_array_cgs))
     mfall = np.zeros(len(tfb_cgs))
@@ -105,7 +108,8 @@ for j, check in enumerate(checks):
         mfall[i] = mdot # code units
         mdot_cgs = mdot * prel.Msol_cgs / prel.tsol_cgs # [CGS]
         # Find the luminosity at time t
-        idx = np.argmin(np.abs(snaps[i]-snapsLum)) # just to be sure that you match the data
+        idx = np.argmin(np.abs(tfb[i]-tfbLum)) # just to be sure that you match the data
+        # idx = np.argmin(np.abs(snaps[i]-snapsLum)) # just to be sure that you match the data
         Lum_t = Lum[idx]
         eta_sh[i] = efficiency_shock(Lum_t, mdot_cgs, prel.c_cgs) # [CGS]
         R_sh[i] = R_shock(Mbh_cgs, eta_sh[i], prel.G_cgs, prel.c_cgs) # [CGS]

src/coloredE.py
@@ -48,7 +48,7 @@ Rs = 2*G*Mbh / c**2
 Rt = Rstar * (Mbh/mstar)**(1/3)
 Rp =  Rt / beta
 R0 = 0.6 * Rt
-apo = Rt**2 / Rstar #2 * Rt * (Mbh/mstar)**(1/3)
+apo = orb.apocentre(Rstar, mstar, Mbh, beta)
 
 
 radii = np.logspace(np.log10(R0), np.log10(1.5*apo),