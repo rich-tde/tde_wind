cba68f5876410763e0230fd9c3a66ad332d68392
spaceturtle707 <p.martire98@gmail.com>
2025-08-12 09:31:14 +0200

    single_branch

--- Utilities/operators.py
+++ Utilities/operators.py
@@ -264,7 +264,7 @@ def single_branch(radii, R, tocast, weights, keep_track = False):
             width = radii[i] - radii[i-1]
         else:
             width = (radii[i+1] - radii[i-1]) / 2
-        width *= 1.5 # make it slightly bigger to smooth things
+        # width *= 1.5 # make it slightly bigger to smooth things
         # indices = tree.query_ball_point(radius, width) #if KDTree from scipy
         indices = tree.query_radius(radius, width) #if KDTree from sklearn
         indices = np.concatenate(indices)
