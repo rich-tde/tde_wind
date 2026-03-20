correct cos weighting in spectra

plotting/spectra.py
@@ -36,33 +36,32 @@ N_obs = F_photo.shape[0]
 observers_xyz = hp.pix2vec(prel.NSIDE, range(N_obs)) #shape: (3, 192)
 observers_xyz = np.array(observers_xyz)
 
-indices_sorted, label_obs, colors_obs, lines_obs = choose_observers(observers_xyz, choice = 'left_right_in_out_z')
-# indices_sorted = np.concatenate(indices_sorted)
+# indices_sorted, label_obs, colors_obs, lines_obs = choose_observers(observers_xyz, choice = 'left_right_in_out_z')
 
-fig, ax = plt.subplots(1, 1, figsize=(8,6))
-F_mean = []
-for i, idx_list in enumerate(indices_sorted):
-    F_mean.append(np.mean(F_photo[idx_list], axis=0))
+# fig, ax = plt.subplots(1, 1, figsize=(8,6))
+# F_mean = []
+# for i, idx_list in enumerate(indices_sorted):
+#     F_mean.append(np.mean(F_photo[idx_list], axis=0))
 
-for idx, lab in enumerate(label_obs):
-    if x_axis == 'Freq':
-        ax.plot(freqs, freqs * F_mean[idx], c = colors_obs[idx], label = lab)
-        ax.set_xlabel('Frequency [Hz]')
-        ax.set_xlim(1e14, 1e19)
-    elif x_axis == 'Temp':  
-        ax.plot(Temp, freqs * F_mean[idx], c = colors_obs[idx], label = lab)
-        ax.set_xlabel('Temperature [K]')
-        ax.set_xlim(1e3, 1e8)
-ax.tick_params(axis='both', which='major', length=8, width=1.2)
-ax.tick_params(axis='both', which='minor', length=5, width=1)
-ax.loglog()
-ax.set_ylim(1e38, 1e43)
-ax.set_ylabel(r'$\nu F_{\nu}$ [erg s$^{-1}$ cm$^{-2}$]')
-ax.legend()
-ax.set_title(f't = {time:.2f}'+ r' t$_{\rm fb}$', fontsize=20)
-plt.tight_layout()
+# for idx, lab in enumerate(label_obs):
+#     if x_axis == 'Freq':
+#         ax.plot(freqs, freqs * F_mean[idx], c = colors_obs[idx], label = lab)
+#         ax.set_xlabel('Frequency [Hz]')
+#         ax.set_xlim(1e14, 1e19)
+#     elif x_axis == 'Temp':  
+#         ax.plot(Temp, freqs * F_mean[idx], c = colors_obs[idx], label = lab)
+#         ax.set_xlabel('Temperature [K]')
+#         ax.set_xlim(1e3, 1e8)
+# ax.tick_params(axis='both', which='major', length=8, width=1.2)
+# ax.tick_params(axis='both', which='minor', length=5, width=1)
+# ax.loglog()
+# ax.set_ylim(1e38, 1e43)
+# ax.set_ylabel(r'$\nu F_{\nu}$ [erg s$^{-1}$ cm$^{-2}$]')
+# ax.legend()
+# ax.set_title(f't = {time:.2f}'+ r' t$_{\rm fb}$', fontsize=20)
+# plt.tight_layout()
 
-fig, ax = plt.subplots(1, 1, figsize=(8,6))
+fig, ax = plt.subplots(1, 1, figsize=(10,6))
 for idx, F in enumerate(F_photo):
     if idx not in [0, 191]:
         continue
@@ -77,7 +76,7 @@ for idx, F in enumerate(F_photo):
 ax.tick_params(axis='both', which='major', length=8, width=1.2)
 ax.tick_params(axis='both', which='minor', length=5, width=1)
 ax.loglog()
-ax.set_ylim(1e38, 1e44)
+ax.set_ylim(1e38, 1e42)
 ax.set_ylabel(r'$\nu F_{\nu}$ [erg s$^{-1}$]')
 ax.legend()
 ax.set_title(f't = {time:.2f}'+ r' t$_{\rm fb}$', fontsize=20)

src/Opacity/linextrapolator.py
@@ -55,10 +55,11 @@ def opacity_extrap(x, y, K, which_opacity, scatter = None, slope_length = 7,  ex
                 if ysel < y[0]: # Too rarefied
                     deltay = y[slope_length - 1] - y[0]
                     Kxslope = (K[slope_length - 1, 0] - K[0, 0]) / deltax
-                    if which_opacity == 'planck':
-                        Kyslope = special_rho_slope
-                    else:
-                        Kyslope = (K[0, slope_length - 1] - K[0, 0]) / deltay
+                    # if which_opacity == 'planck':
+                    #     Kyslope = special_rho_slope
+                    # else:
+                    #     Kyslope = (K[0, slope_length - 1] - K[0, 0]) / deltay
+                    Kyslope = (K[0, slope_length - 1] - K[0, 0]) / deltay
                     Kn[ix][iy] = K[0, 0] + Kxslope * (xsel - x[0]) + Kyslope * (ysel - y[0])
                 
                     if scatter is not None:
@@ -82,10 +83,10 @@ def opacity_extrap(x, y, K, which_opacity, scatter = None, slope_length = 7,  ex
                 Kxslope = highT_slope 
                 if ysel < y[0]: # Too rarefied
                     deltay = y[slope_length - 1] - y[0]
-                    if which_opacity == 'planck':
-                        Kyslope = special_rho_slope
-                    else:
-                        Kyslope = (K[-1, slope_length - 1] - K[-1, 0]) / deltay
+                    # if which_opacity == 'planck':
+                    #     Kyslope = special_rho_slope
+                    # else:
+                    Kyslope = (K[-1, slope_length - 1] - K[-1, 0]) / deltay
                     Kn[ix][iy] = K[-1, 0] + Kxslope * (xsel - x[-1]) + Kyslope * (ysel - y[0])
                     
                     if scatter is not None:
@@ -101,7 +102,6 @@ def opacity_extrap(x, y, K, which_opacity, scatter = None, slope_length = 7,  ex
                     iy_inK = np.argmin(np.abs(y - ysel))
                     Kn[ix][iy] = K[-1, iy_inK] + Kxslope * (xsel - x[-1])
                 
-
             else: 
                 ix_inK = np.argmin(np.abs(x - xsel))
                 if ysel < y[0]: # Too rarefied, Temperature is inside table

src/Wind/polarization.py
@@ -70,7 +70,7 @@ def compute_polarization(xph, yph, zph, dimph,
     # Positions
     r_vec = np.vstack((xph, yph, zph)).T # shape: (192,3)
     r_mag = np.linalg.norm(r_vec, axis=1)
-    n_hat = r_vec / r_mag[:, None]   # surface normal (radial approx)
+    r_hat = r_vec / r_mag[:, None]   # surface normal (radial approx)
 
     # Flux vectors
     F_vec = np.vstack((Fxph, Fyph, Fzph)).T
@@ -82,7 +82,7 @@ def compute_polarization(xph, yph, zph, dimph,
     F_hat[valid] = F_vec[valid] / F_mag[valid][:, None]
 
     # Visibility condition
-    mu_surface = np.dot(n_hat, n)
+    mu_surface = np.dot(r_hat, n)
     visible = mu_surface > 0
     # Keep only visible patches
     F_hat, F_mag, mu_surface, r_mag, dimph = make_slices([F_hat, F_mag, mu_surface, r_mag, dimph], visible)
@@ -93,19 +93,19 @@ def compute_polarization(xph, yph, zph, dimph,
     # Thomson polarization fraction for the  cell
     P_local = (1 - cos_theta**2) / (1 + cos_theta**2)
 
-    # --- Define sky plane basis vectors: k, e1, e2 ---
-    # Choose arbitrary reference axis not parallel to k to find e1
+    # --- Define a (fixed, arbitrary) sky basis with a plane perpendicular to the line-of-sight direction n
+    # vectors: n, e1, e2
     tmp = np.array([1.0, 0.0, 0.0])
-    if np.allclose(np.abs(np.dot(tmp, n)), 1.0):
-        tmp = np.array([0.0, 1.0, 0.0])
     e1 = np.cross(n, tmp)
-
+    if np.linalg.norm(e1) < 1e-6:   # avoid degeneracy if n || tmp
+        tmp = np.array([0.0, 1.0, 0.0])
+        e1 = np.cross(n, tmp)
     e1 /= np.linalg.norm(e1)
     e2 = np.cross(n, e1)
 
-    # Polarization direction vector, perpendicular to the scattering plane
-    e_pol = np.cross(F_hat, n)
-    # e_pol = np.cross(n, cross1)
+    # Polarization direction vector, in the scattering plane, orthogonal to n
+    cross1 = np.cross(F_hat, n)
+    e_pol = np.cross(n, cross1)
 
     # Project polarization direction onto sky plane
     e_pol_mag = np.linalg.norm(e_pol, axis=1)
@@ -119,15 +119,17 @@ def compute_polarization(xph, yph, zph, dimph,
     cos2phi = cos_phi**2 - sin_phi**2
     sin2phi = 2 * cos_phi * sin_phi
 
-    # Surface area weight (uniform sphere sampling assumption)
-    # dOmega = 4*np.pi / N
-    # dA = r_mag**2 * dOmega
-    dA = np.pi * dimph**2  
+    # Surface area weight 
+    # uniform sphere sampling assumption: dOmega = 4*np.pi / N,  dA = r_mag**2 * dOmega
+    # dA = np.pi * dimph**2  
+    # dA_proj = dA * mu_surface  # projected area toward observer
 
     # Intensity weighted by the area projected toward the observer
-    I_local = F_mag * mu_surface * dA
+    # I_local = F_mag * dA_proj
+    I_local = F_mag # otherwise I has the wrong units
 
-    # Stokes parameters
+    # Stokes parameters. 
+    # I_local * P_local make you consider only the light that is locally scattered
     Q = np.sum(I_local * P_local * cos2phi)
     U = np.sum(I_local * P_local * sin2phi)
     I = np.sum(I_local)

src/Wind/wind_profiles.py
@@ -48,111 +48,6 @@ Ledd_cgs = Ledd_sol * prel.en_converter/prel.tsol_cgs
 Medd_cgs = Medd_sol * prel.Msol_cgs/prel.tsol_cgs
 
 #%% FUNCTIONS
-def CouBegel(r, theta, n, norm, gamma=4/3):
-    """Coughlin+14, eq.4"""
-    r0, rho0 = norm
-    q = 3/2 - n
-    alpha = (1-q*(gamma-1))/(gamma-1)
-    rho = rho0 *(r/r0)**(-q) * np.sin(theta)**(2*alpha)
-    return rho
-
-def radial_profiles_old(loadpath, snap, ray_params, choice):
-    rmin, rmax, Nray = ray_params
-    r_array = np.logspace(np.log10(rmin), np.log10(rmax), Nray)
-    data = op.make_tree(loadpath, snap, energy = True)
-    X, Y, Z, Vol, Den, Mass, VX, VY, VZ, T, Press, IE_den, Rad_den = \
-        data.X, data.Y, data.Z, data.Vol, data.Den, data.Mass, data.VX, data.VY, data.VZ, data.Temp, data.Press, data.IE, data.Rad
-    cut = Den > 1e-19
-    X, Y, Z, Vol, Den, Mass, VX, VY, VZ, T, Press, IE_den, Rad_den = \
-        make_slices([X, Y, Z, Vol, Den, Mass, VX, VY, VZ, T, Press, IE_den, Rad_den], cut)
-    Rsph = np.sqrt(X**2 + Y**2 + Z**2)  
-    dim_cell = Vol**(1/3)
-    
-    # split in sections all the cells
-    sections_in = op.choose_sections(X, Y, Z, choice = choice)
-    Rsph_initial = []
-    dim_cell_initial = []
-    for key in sections_in.keys():
-        cond_sec = sections_in[key]['cond']
-        Rsph_initial.append(Rsph[cond_sec])
-        dim_cell_initial.append(dim_cell[cond_sec])
-
-    cut, bern, V_r = orb.pick_wind(X, Y, Z, VX, VY, VZ, Den, Mass, Press, IE_den, Rad_den, params)
-    X, Y, Z, Rsph, Vol, Den, Mass, V_r, T, Press, IE_den, Rad_den, dim_cell, bern = \
-        make_slices([X, Y, Z, Rsph, Vol, Den, Mass, V_r, T, Press, IE_den, Rad_den, dim_cell, bern], cut)       
-    indices_all = np.arange(len(X))
-    
-    # split in sections yhe wind cells
-    sections = op.choose_sections(X, Y, Z, choice = choice)
-    ind_sec = []
-    colors_obs = []
-    label_obs = []
-    lines_obs = []
-    for key in sections.keys():
-        cond_sec = sections[key]['cond']
-        ind_sec.append(indices_all[cond_sec])
-        colors_obs.append(sections[key]['color'])
-        label_obs.append(sections[key]['label'])
-        lines_obs.append(sections[key]['line'])
-
-    all_outflows = {}
-    const_C = 4/len(ind_sec)
-    for j, ind in enumerate(ind_sec):
-        t_prof = np.zeros(Nray)
-        v_rad_prof = np.zeros(Nray)
-        d_prof = np.zeros(Nray)
-        Mdot_prof = np.zeros(Nray)
-        Mdotmean_prof = np.zeros(Nray)
-        L_kin_prof = np.zeros(Nray)
-        L_adv_prof = np.zeros(Nray)
-        ratio_un = np.zeros(Nray)
-
-        Rsph_initial_j = Rsph_initial[j]
-        dim_cell_initial_j = dim_cell_initial[j]
-        
-        for i, r in enumerate(r_array): 
-            # find cells at r
-            cond_r_initial = np.abs(Rsph_initial_j-r) < dim_cell_initial_j
-            ind_r = indices_all[np.abs(Rsph-r) < dim_cell]
-            ind_sec_r = np.intersect1d(ind_r, ind)
-            if len(ind_sec_r) == 0:
-                continue
-            ray_V_r = V_r[ind_sec_r] 
-            ray_d = Den[ind_sec_r] 
-            ray_m = Mass[ind_sec_r]
-            ray_rad_den = Rad_den[ind_sec_r]
-            ray_vol = Vol[ind_sec_r]
-            ray_dim = dim_cell[ind_sec_r]
-            ray_t = (ray_rad_den * prel.en_den_converter / prel.alpha_cgs)**(1/4) 
-            L_adv =  ray_V_r * ray_rad_den
-            t_prof[i] = np.sum(ray_t*ray_vol) / np.sum(ray_vol)
-            v_rad_prof[i] = np.sum(ray_V_r*ray_m) / np.sum(ray_m)
-            d_prof[i] = np.sum(ray_d*ray_m)/ np.sum(ray_m)
-            Mdot_prof[i] = const_C * r**2 / np.sum(ray_dim**2) * np.pi * np.sum(ray_dim**2 * ray_d * ray_V_r)
-            Mdotmean_prof[i] = 4 * np.pi * r**2 * np.mean(ray_d * ray_V_r) if ray_V_r.size > 0 else 0 
-            L_kin_prof[i] = const_C * r**2 / np.sum(ray_dim**2)* 0.5 * np.pi *np.sum(ray_dim**2 * ray_d * ray_V_r**3)
-            L_adv_prof[i] = const_C * np.pi * r**2 * np.mean(L_adv)
-            ratio_un[i] = len(ray_d) / len(Rsph_initial_j[cond_r_initial]) if len(Rsph_initial_j[cond_r_initial]) > 0 else 0
-
-        outflow = {
-            'r': r_array,
-            't_prof': t_prof,
-            'v_rad_prof': v_rad_prof,
-            'd_prof': d_prof,
-            'Mdot_prof': Mdot_prof,
-            'Mdotmean_prof': Mdotmean_prof,
-            'L_adv_prof': L_adv_prof,
-            'L_kin_prof': L_kin_prof,
-            'ratio_un': ratio_un,
-            'colors_obs': colors_obs[j],
-            'lines_obs': lines_obs[j]
-        }
-
-        key = f"{label_obs[j]}"
-        all_outflows[key] = outflow
-    
-    return all_outflows
-
 def radial_profiles(loadpath, snap, ray_params, choice):
     rmin, rmax, Nray = ray_params
     r_array = np.logspace(np.log10(rmin), np.log10(rmax), Nray)
@@ -164,15 +59,6 @@ def radial_profiles(loadpath, snap, ray_params, choice):
         make_slices([X, Y, Z, Vol, Den, Mass, VX, VY, VZ, T, Press, IE_den, Rad_den], cut)
     Rsph = np.sqrt(X**2 + Y**2 + Z**2)  
     dim_cell = Vol**(1/3)
-    
-    # split in sections all the cells
-    sections_in = op.choose_sections(X, Y, Z, choice = choice)
-    Rsph_initial = []
-    dim_cell_initial = []
-    for key in sections_in.keys():
-        cond_sec = sections_in[key]['cond']
-        Rsph_initial.append(Rsph[cond_sec])
-        dim_cell_initial.append(dim_cell[cond_sec])
 
     cut, bern, V_r = orb.pick_wind(X, Y, Z, VX, VY, VZ, Den, Mass, Press, IE_den, Rad_den, params)
     X, Y, Z, Rsph, Vol, Den, Mass, V_r, T, Press, IE_den, Rad_den, dim_cell, bern = \
@@ -191,7 +77,7 @@ def radial_profiles(loadpath, snap, ray_params, choice):
         label_obs.append(sections[key]['label'])
         lines_obs.append(sections[key]['line'])
 
-    shell_indices = [[] for _ in range(Nray)]
+    shell_indices = [0, 191]
     for i, r in enumerate(r_array): 
         # find cells at r
         ind_r = np.abs(Rsph-r) < dim_cell

src/fld_curve.py
@@ -31,7 +31,7 @@ import Utilities.prelude as prel
 from Utilities.selectors_for_snap import select_snap, select_prefix
 from Utilities.sections import make_slices
 import src.orbits as orb
-from Utilities.operators import make_tree, to_spherical_components
+from Utilities.operators import make_tree
 
 #%% Choose parameters -----------------------------------------------------------------
 m = 4
@@ -59,7 +59,8 @@ planck = np.loadtxt(f'{opac_path}/planck.txt')
 scattering = np.loadtxt(f'{opac_path}/scatter.txt') # 1/cm
 _, _, scatter2 = opacity_linear(T_cool, Rho_cool, scattering)
 T_cool2, Rho_cool2, rossland2 = opacity_extrap(T_cool, Rho_cool, rossland, which_opacity = 'rossland', scatter = scatter2)
-_, _, planck2 = opacity_extrap(T_cool, Rho_cool, planck, which_opacity = 'planck', scatter = None)
+_, _, planck2 = opacity_extrap(T_cool, Rho_cool, planck, which_opacity = 'planck', slope_length = 10, scatter = None)
+
 # observers 
 num_obs = prel.NPIX
 observers_xyz = hp.pix2vec(prel.NSIDE, range(num_obs)) # shape: (3, 192)
@@ -67,11 +68,11 @@ observers_xyz = np.array(observers_xyz).T # shape: (192, 3)
 # Cross dot -----------------------------------------------------------------
 cross_dot = np.matmul(observers_xyz,  observers_xyz.T)
 cross_dot[cross_dot<0] = 0
-cross_dot *= 4/192
+cross_dot /= 192
 #%% MATLAB, thanks Cindy.
 eng = matlab.engine.start_matlab()
 for idx_s, snap in enumerate(snaps):
-    if snap not in [74, 75,76, 77]:
+    if snap != 109:
         continue
     print('\n Snapshot: ', snap, '\n', flush=True)
     # Load data and avoid fluff -----------------------------------------------------------------
@@ -294,7 +295,6 @@ for idx_s, snap in enumerate(snaps):
         colorsphere['P'].append(ray_press[color_idx])
         colorsphere['ieden'].append(ray_ie_den[color_idx])
         colorsphere['alpha_eff'].append(alpha_effective[color_idx])
-        # print(Lphoto, r[color_idx])
 
         # Spectra ---
         for k in range(color_idx, len(r)):
@@ -308,33 +308,47 @@ for idx_s, snap in enumerate(snaps):
         
         norm = Lphoto / np.trapezoid(L_col_temp[i,:], prel.freqs)
         L_col_temp[i,:] *= norm
-        L_col[i,:] = np.dot(cross_dot[i,:], L_col_temp)
+        # L_col[i,:] = np.dot(cross_dot[i,:], L_col_temp)
          
         # if plot:
         #     Rt = 13
-        #     plt.figure(figsize = (8, 6))
-        #     plt.plot(r/Rt, los, label = f'obs {i}, photo', ls = '--', c = 'b')
-        #     plt.plot(r/Rt, los_effective, label = f'obs {i}, col')
-        #     plt.axvline(rph[i]/Rt, ls = '--', c = 'b')
-        #     plt.axvline(r[color_idx]/Rt)
-        #     plt.axhline(5, color = 'grey', linestyle = '--')
-        #     plt.xlim(2,80)
-        #     plt.loglog()
-        #     plt.xlabel(r'$r/r_{\rm t}$')
-        #     plt.ylabel(r'$\kappa$ [cm$^2$/g]')
-        #     plt.ylim(1e-1, 1e2) 
-        #     plt.grid()
+        #     fig, (ax1, ax2, ax3) = plt.subplots(3, 1, figsize = (8, 16))
+        #     ax1.plot(r/Rt, los, label = f'obs {i}, photo', ls = '--', c = 'b')
+        #     ax1.plot(r/Rt, los_effective, label = f'obs {i}, col', c = 'r')
+        #     ax1.axvline(rph[i]/Rt, ls = '--', c = 'b')
+        #     ax1.axvline(r[color_idx]/Rt, c = 'r')
+        #     ax1.axhline(5, color = 'grey', linestyle = '--')
+        #     ax1.set_ylabel(r'$\kappa$ [cm$^2$/g]')
+        #     ax1.set_ylim(1e-1, 5e2)
+
+        #     ax2.plot(r/Rt, t, label = f'obs {i}, photo', c = 'k')
+        #     ax2.axvline(rph[i]/Rt, ls = '--', c = 'b', label = f'obs {i}, photo')
+        #     ax2.axvline(r[color_idx]/Rt, c = 'r',  label = f'obs {i}, col')
+        #     ax2.set_ylabel(r'T [K]')
+        #     ax2.set_ylim(2e4, 1e9)
+
+        #     ax3.plot(r/Rt, d, label = f'obs {i}, photo', c = 'k')
+        #     ax3.axvline(rph[i]/Rt, ls = '--', c = 'b', label = f'obs {i}, photo')
+        #     ax3.axvline(r[color_idx]/Rt, c = 'r', label = f'obs {i}, col')
+        #     ax3.set_ylabel(r'Den [g/cm$^3$]')
+        #     ax3.set_ylim(1e-15, 1e-12)
+
+        #     for ax in [ax1, ax2, ax3]:
+        #         ax.set_xlim(2,80)
+        #         ax.loglog()
+        #         ax.set_xlabel(r'$r/r_{\rm t}$')
+        #         ax.grid()
+        #         ax.legend(fontsize = 16)
+        #         ax.tick_params(axis='both', which='major',length=10, width=1.5)
+        #         ax.tick_params(axis='both', which='minor',length=5, width=1)
         #     plt.tight_layout() 
-        #     plt.legend(fontsize = 16)
-        #     plt.tick_params(axis='both', which='major',length=10, width=1.5)
-        #     plt.tick_params(axis='both', which='minor',length=5, width=1)
             # plt.savefig(f'{abspath}/Figs/{folder}/Test/{snap}/alphai_{snap}_{i}.png')
             # plt.close()
 
         del smoothed_flux_r2, R_lamda, fld_factor, ray_radDen
         gc.collect()
 
-    # L_col = np.matmul(cross_dot, L_col_temp)
+    L_col = np.matmul(cross_dot, L_col_temp)
     Lphoto_snap = np.mean(Lph) # take the mean
     print('L :', Lphoto_snap, flush=True)
 
@@ -381,9 +395,9 @@ for idx_s, snap in enumerate(snaps):
         #     f.close()
 
         # Save spectrum
-        np.savetxt(f'{pre_saving}/spectra/freqs.txt', prel.freqs)
-        np.savetxt(f'{pre_saving}/spectra/{check}_spectra{snap}.txt', L_col)
-        np.savez(f"{pre_saving}/spectra/{check}_Rcol{snap}.npz", **colorsphere)
+        np.savetxt(f'{pre_saving}/spectra_cos/freqs.txt', prel.freqs)
+        np.savetxt(f'{pre_saving}/spectra_cos/{check}_spectra{snap}.txt', L_col)
+        np.savez(f"{pre_saving}/spectra_cos/{check}_Rcol{snap}.npz", **colorsphere)
         
             
     del xph, yph, zph, volph, denph, Tempph, Rad_denph, Vxph, Vyph, Vzph, Pressph, IE_denph, rph, alphaph, Lph, ph_idx